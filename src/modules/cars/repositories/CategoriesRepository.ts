a84515dec1673dca09353922dcb644ae982202f6
lopessoftec <lopessoftec@gmail.com>
2022-11-15 19:04:32 -0300

    Conhecimento singleton patten

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -4,9 +4,19 @@ import { ICategoriesRepository, ICreateCategoryDTO } from './ICategoriesReposito
 
 class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[];
+    private static INSTANCE: CategoriesRepository;
 
-    constructor() {
+    private constructor() {
         this.categories = [];
+    }
+
+    public static getInstance(): CategoriesRepository {
+
+        if (!CategoriesRepository.INSTANCE) {
+            CategoriesRepository.INSTANCE = new CategoriesRepository();
+        }
+
+        return CategoriesRepository.INSTANCE;
     }
 
     create({ name, description }: ICreateCategoryDTO): void {
