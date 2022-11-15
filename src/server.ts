d7585c693681fe67984d83b7a26e909af46541cf
lopessoftec <lopessoftec@gmail.com>
2022-11-15 09:06:39 -0300

    Debugando a aplicação

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,17 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 // app.get("/", createCourse);
 
 app.get("/", (request, response) => {
     return response.json({ message: "Hello World" });
 });
+
+app.post("/courses", (request, response) => {
+    const { name } = request.body;
+    return response.json({ name });
+})
 
 app.listen(3333);
