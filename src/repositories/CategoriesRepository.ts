c4dce67eab9e0d3fbf7b59137e9415c6a403923e
lopessoftec <lopessoftec@gmail.com>
2022-11-15 10:58:58 -0300

    Criando repositorio de categoria

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,31 @@
+import { Category } from '../model/Category';
+
+// DTO => Data transfer object
+interface ICreateCategoryDTO {
+    name: string;
+    description: string;
+}
+
+class CategoriesRepository {
+
+    private categories: Category[];
+
+    constructor() {
+        this.categories = [];
+    }
+
+    create({ name, description }: ICreateCategoryDTO): void {
+        const category = new Category();
+
+        // Consigo passar os atributos para meu category
+        Object.assign(category, {
+            name,
+            description,
+            created_at: new Date()
+        });
+
+        this.categories.push(category);
+    }
+}
+
+export { CategoriesRepository }
