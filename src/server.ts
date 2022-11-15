70fadeeb57b2ec6ed96ae79afd2698b4d0ce1bd9
lopessoftec <lopessoftec@gmail.com>
2022-11-15 09:35:33 -0300

    Criando categoria

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,10 @@
 import express from "express";
-
-// import { createCourse } from "./routes";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-// app.get("/", createCourse);
+app.use(categoriesRoutes);
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World" });
-});
-
-app.post("/courses", (request, response) => {
-    const { name } = request.body;
-    return response.json({ name });
-})
-
-app.listen(3333);
+app.listen(3333, () => console.log("Server is running!"));
