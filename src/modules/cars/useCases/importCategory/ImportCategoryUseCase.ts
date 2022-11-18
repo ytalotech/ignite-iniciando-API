67e19c067f00b56c19aaa567143f7af72f49fe74
lopessoftec <lopessoftec@gmail.com>
2022-11-18 08:14:48 -0300

    Conhecendo o conceito de stream e deletando tmp do git

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,20 @@
+import { parse as csvParse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-    execute(file: any) {
-        console.log(file);
+    execute(file: Express.Multer.File): void {
+
+        const stream = fs.createReadStream(file.path);
+
+        // Responsavel por ler linha por linha em nosso arquivo
+        const parseFile = csvParse();
+
+        // pipe pega nosso pedaço lido e passa para o parse file
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async (line) => {
+            console.log(line)
+        })
     }
 }
 
