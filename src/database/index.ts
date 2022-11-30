9aa539ecb5e32ab292a5db6ed29f8c8833621bb8
lopessoftec <lopessoftec@gmail.com>
2022-11-29 21:00:41 -0300

    Criando container do postgres

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,13 @@
-import { DataSource } from "typeorm";
+import { createConnection, getConnectionOptions } from 'typeorm';
 
-const dataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "docker",
-    password: "1234",
-    database: "rentx"
+interface IOptions {
+    host: string;
+}
+
+getConnectionOptions().then(options => {
+    const newOptions = options as IOptions;
+    newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+    createConnection({
+        ...options,
+    });
 });
-
-dataSource.initialize();
