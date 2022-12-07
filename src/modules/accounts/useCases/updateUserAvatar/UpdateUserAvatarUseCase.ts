58c1b44eba761c3284b6f8dfd3bf500fde086d6f
lopessoftec <lopessoftec@gmail.com>
2022-12-06 22:03:13 -0300

    Adicionando coluna de avatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    user_id: string;
+    avatar_file: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+    // Adicionar coluna avatar na tabela de users
+    // Refatorar usuario com coluna avatar
+    // Configuração upload multer
+    // Criar regra de negocio do upload
+    // Criar controller
+    constructor(
+        @inject("UsersRepository")
+        private UsersRepository: IUsersRepository
+    ) { }
+
+    async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+        const user = await this.UsersRepository.findById(user_id);
+
+        user.avatar = avatar_file;
+
+        await this.UsersRepository.create(user);
+    }
+}
+
+export { UpdateUserAvatarUseCase }
