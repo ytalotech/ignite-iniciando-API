fa4b572f0de6f547158cfaadc94c957c347dc08a
lopessoftec <lopessoftec@gmail.com>
2022-12-21 22:02:02 -0300

    Refatorando a aplicacao

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -1,8 +1,8 @@
+import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { getRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
 
-import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
-import { User } from "../../entities/User";
-import { IUsersRepository } from "../IUsersRepository";
 
 class UsersRepository implements IUsersRepository {
     private respository: Repository<User>;
