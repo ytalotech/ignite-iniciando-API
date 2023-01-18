d8edfc5fff8abcfb019f015e4dd6706a19cc4261
lopessoftec <lopessoftec@gmail.com>
2023-01-17 21:36:34 -0300

    Continuando teste de integracao

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -6,7 +6,7 @@ export default async (host = "database"): Promise<Connection> => {
     return createConnection(
         Object.assign(defaultOptions, {
             host: process.env.NODE_ENV === "test" ? "localhost" : host,
-            database: process.env.NODE_ENV === 'test' ? "rentex_test" : defaultOptions.database
+            database: process.env.NODE_ENV === 'test' ? "rentx_test" : defaultOptions.database
         })
     )
 }
