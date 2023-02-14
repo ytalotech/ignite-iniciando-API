d02b2e5a87151f404869da62ac91e2a750ebf238
lopessoftec <lopessoftec@gmail.com>
2023-02-13 21:34:00 -0300

    Corrigindo o refresh token

--- src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -10,6 +10,11 @@ interface IPayload {
     email: string;
 }
 
+interface ITokenResponse {
+    token: string;
+    refresh_token: string;
+}
+
 @injectable()
 class RefreshTokenUseCase {
     constructor(
@@ -19,7 +24,7 @@ class RefreshTokenUseCase {
         private dateProvider: IDateProvider
     ) { }
 
-    async execute(token: string): Promise<string> {
+    async execute(token: string): Promise<ITokenResponse> {
         // retorno esperado é um  as IPayload
         const { email, sub } = verify(token, auth.secret_refresh_token) as IPayload;
 
@@ -48,7 +53,15 @@ class RefreshTokenUseCase {
             user_id
         })
 
-        return refresh_token;
+        const newToken = sign({}, auth.secret_token, {
+            subject: user_id,
+            expiresIn: auth.expires_in_token
+        })
+
+        return {
+            refresh_token,
+            token: newToken
+        }
     }
 }
 
