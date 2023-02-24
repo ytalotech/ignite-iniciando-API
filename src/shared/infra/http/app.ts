b86d772073c5a74951773f41af196d537c2a0ad2
lopessoftec <lopessoftec@gmail.com>
2023-02-23 22:07:30 -0300

    configurando Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,26 +1,48 @@
 import "reflect-metadata";
 import "dotenv/config";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
-import cors from "cors";
-
-import createConnection from "@shared/infra/typeorm";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 import "@shared/infra/typeorm";
 
 import "@shared/container";
 import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
-import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 
 createConnection();
 const app = express();
 
 app.use(rateLimiter);
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
@@ -30,7 +52,9 @@ app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`)); //é para ele 
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
-app.use(router)
+app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
